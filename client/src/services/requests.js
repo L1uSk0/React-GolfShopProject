4f2b1b229aa14e2097818e4a8bdb1c85c9c08765
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-21 17:56:18 +0200

    adding error handling for the requester

--- client/src/services/requests.js
+++ client/src/services/requests.js
@@ -15,16 +15,32 @@ const request = async (method, url, data, options = {}) => {
         }
     }
 
-    const response = await fetch(url, options);
-    const responseContentType = response.headers.get('Content-Type');
-    if (!responseContentType) {
-        return;
-    }
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok){
+            const errorData = await response.json();
+            if(response.status === 403 && errorData.message == "Invalid access token"){
+                localStorage.clear();
+            }
+            throw new Error(errorData.message || "Something went wrong");
+        }
+        if (response.status === 204) {
+            return response;
+        }
+        const responseContentType = response.headers.get('Content-Type');
+        if (!responseContentType) {
+            return;
+        }
+        
+        const result = await response.json();
     
-    const result = await response.json();
+        return result;
 
-    return result;
-
+    } catch (error) {
+        console.error("API Request Error:", error.message || error);
+        alert(error.message || "An unexpected error occurred."); 
+        throw error; 
+    }
 };
 
 export default {
