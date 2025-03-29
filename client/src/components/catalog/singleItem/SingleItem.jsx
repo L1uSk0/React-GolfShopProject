eb6d5e6ba83d5c4869962910d7e0a5d3a06d1d01
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-30 01:37:07 +0200

    fix bug where id was given incorrectly

--- client/src/components/catalog/singleItem/SingleItem.jsx
+++ client/src/components/catalog/singleItem/SingleItem.jsx
@@ -2,19 +2,19 @@ import { Link } from "react-router";
 
 
 export default function SingleItem({
-    id,
+    _id,
     img,
     name,
     price
 }) {
     return (
         <>
-            <div key={id} className="catalog-item">
+            <div key={_id} className="catalog-item">
                 <img src={img} alt={name} />
                 <h3>{name}</h3>
                 <p>{price}</p>
                 <button className="add tocard">Add to Cart</button>
-                <Link to={`/items/${id}/details`} className="details-button">Details</Link>
+                <Link to={`/items/${_id}/details`} className="details-button">Details</Link>
             </div>
         </>
     );
