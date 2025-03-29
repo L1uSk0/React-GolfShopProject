37efa7b62098688b41f01baf2b18ee5aafc62138
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-30 01:36:37 +0200

    ItemDetails - functionality + css fix

--- client/src/components/details/ItemDetails.jsx
+++ client/src/components/details/ItemDetails.jsx
@@ -1,22 +1,52 @@
 import React from "react";
 import "./ItemDetails.css";
+import { Link, useNavigate, useParams } from "react-router";
+import useAuth from "../../hooks/useAuth.js";
+import { useDeleteItem, useItem } from "../../api/itemApi.js";
+
 
 export default function ItemDetails() {
+    const navigate = useNavigate();
+    const { itemId } = useParams();
+    console.log(itemId);
+    const { userId } = useAuth();
+    const { item } = useItem(itemId);
+    const { deleteItem } = useDeleteItem();
+
+    console.log(item);
+
+    const itemDeleteHandler = async () => {
+        const confirmation = confirm(`Are you sure you want to delete ${item.name} item ?`)
+        if (!confirmation) {
+            return;
+        }
+
+        await deleteItem(itemId)
+
+        navigate('/items')
+    }
+
+    const isOwner = userId === item._ownerId
 
 
     return (
         <>
             <div className="details-overlay">
                 <div className="details-card">
-                    <img src={'hardcode'} alt={'hardcode'} className="details-image" />
-                    <h3>{'hardcode'}</h3>
-                    <p><strong>Price:</strong> {'hardcode'}</p>
-                    <p><strong>Brand:</strong> {"hardcode"}</p>
-                    <p><strong>Model:</strong> {'hardcode'}</p>
-                    <p><strong>Quantity:</strong> {'hardcode'}</p>
-                    <p><strong>Quality:</strong> {'hardcode'}</p>
-                    <button className="edit-button">Edit</button>
-                    <button className="delete-button">Delete</button>
+                    <img src={item.img} alt={item.name} className="details-image" />
+                    <h3>{item.name}</h3>
+                    <p><strong>Price:</strong> {item.price}</p>
+                    <p><strong>Brand:</strong> {item.brand}</p>
+                    <p><strong>Model:</strong> {item.model}</p>
+                    <p><strong>Year:</strong> {item.year}</p>
+                    <p><strong>Quantity:</strong> {item.quantity}</p>
+                    <p><strong>Quality:</strong> {item.quality}</p>
+                    {isOwner && (
+                        <>
+                            <Link to={`/items/${itemId}/edit`} className="button">Edit</Link>
+                            <button className="delete-button" onClick={itemDeleteHandler}>Delete</button>
+                        </>
+                    )}
                     <button className="close-button">Close</button>
                 </div>
             </div>
