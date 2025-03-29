3eda9e47caf4f8d32a71e94e3b3c6905cfc3a331
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-30 00:38:17 +0200

    remodel Catalog and put functionality for api calls

--- client/src/components/createItem/CreateItem.jsx
+++ client/src/components/createItem/CreateItem.jsx
@@ -1,39 +1,81 @@
+import { useNavigate } from "react-router";
+import "./CreateItem.css"
+import { useCreateItem } from "../../api/itemApi.js";
+
+
 export default function CreateItem() {
+    const navigate = useNavigate();
+    const { create:createItem} = useCreateItem();
+
+    const submitAction = async (formData) => {
+        const itemData = Object.fromEntries(formData);
+        
+        await createItem(itemData);
+
+        navigate('/items');
+    }
+
+
     return (
         <>
-            <h1>Sell an Item</h1>
-            <form action="#" method="POST">
-                <label htmlFor="name">Item Name:</label>
-                <input type="text" id="name" name="name" required="" />
-                <label htmlFor="brand">Brand:</label>
-                <input type="text" id="brand" name="brand" required="" />
-                <label htmlFor="model">Model:</label>
-                <input type="text" id="model" name="model" required="" />
-                <label htmlFor="phoneNumber">Phone Number:</label>
-                <input type="tel" id="phoneNumber" name="phoneNumber" required="" />
-                <label htmlFor="img">Image URL:</label>
-                <input type="url" id="img" name="img" required="" />
-                <label htmlFor="year">Year:</label>
-                <input
-                    type="number"
-                    id="year"
-                    name="year"
-                    min={1900}
-                    max={2099}
-                    required=""
-                />
-                <label htmlFor="quantity">Quantity:</label>
-                <input type="number" id="quantity" name="quantity" min={1} required="" />
-                <label htmlFor="quality">Quality:</label>
-                <select id="quality" name="quality" required="">
-                    <option value="excellent">Excellent</option>
-                    <option value="very good">Very Good</option>
-                    <option value="good">Good</option>
-                    <option value="poor">Poor</option>
-                    <option value="bad">Bad</option>
-                </select>
-                <button type="submit">Submit</button>
-            </form>
+            <div className="main-container">
+                <div className="create-item-container">
+                    <h1>Sell an Item</h1>
+                    <form action={submitAction} className="create-item-form">
+                        <div className="form-group">
+                            <label htmlFor="name">Item Name</label>
+                            <input type="text" id="name" name="name" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="brand">Brand</label>
+                            <input type="text" id="brand" name="brand" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="model">Model</label>
+                            <input type="text" id="model" name="model" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="phoneNumber">Phone Number</label>
+                            <input type="tel" id="phoneNumber" name="phoneNumber" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="img">Image URL</label>
+                            <input type="url" id="img" name="img" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="year">Year</label>
+                            <input type="number" id="year" name="year" min={1900} max={2099} required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="quantity">Quantity</label>
+                            <input type="number" id="quantity" name="quantity" min={1} required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="price">Price</label>
+                            <input type="number" id="price" name="price" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="quality">Quality</label>
+                            <select id="quality" name="quality" required>
+                                <option value="excellent">Excellent</option>
+                                <option value="very good">Very Good</option>
+                                <option value="good">Good</option>
+                                <option value="poor">Poor</option>
+                                <option value="bad">Bad</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="submit-btn">Submit</button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
