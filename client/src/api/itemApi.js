f6acb447e397be84afa554073b5ff5cbc07ee543
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-30 00:37:24 +0200

    create itemApi to control all requests for the items

---
+++ client/src/api/itemApi.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react"
+import requests from "../services/requests.js";
+import useAuth from "../hooks/useAuth.js";
+
+const baseUrl = 'http://localhost:3030/data/golfitems'
+
+export const useItems = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        requests.get(baseUrl)
+            .then(setItems)
+    }, [])
+
+    return { items };
+}
+
+export const useItem = (itemId) => {
+    const [item, setItem] = useState({});
+
+    useEffect(() => {
+        requests.get(`${baseUrl}/${itemId}`)
+            .then(setItem)
+    }, [itemId])
+
+    return { item };
+
+}
+
+export const useLatestItems = () => {
+    const [latestItems, setLatestItems] = useState([]);
+
+    useState(() => {
+        const searchParams = new URLSearchParams({
+            sortBy: '_createdOn desc',
+            pageSize: 8,
+            select: '_id,img,price,name'
+        });
+
+        requests.get(`${baseUrl}?${searchParams.toString()}`)
+            .then(setLatestItems);
+    }, []);
+
+    return {latestItems}
+
+}
+
+export const useCreateItem = () => {
+    const { requests } = useAuth();
+
+    const create = (itemData) =>
+        requests.post(baseUrl, itemData)
+
+    return { create }
+
+}
+
+export const useEditItem = () => {
+    const {requests} = useAuth();
+
+    const edit = (itemId,itemData) => 
+        requests.put(`${baseUrl}/${itemId}` , {...itemData, _id:itemId});
+
+    return {edit}
+}
+
+export const useDeleteGame = () => {
+    const {requests} = useAuth();
+
+    const delItem = (itemId) =>
+        requests.delete(`${baseUrl}/${itemId}`);
+
+    return {delItem}
+}
