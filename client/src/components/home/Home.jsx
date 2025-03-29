baa7dd9a93ccf81878380062d3c4e8754bb447a7
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-30 00:39:45 +0200

    remove fake data in Home , fix small CSS issue

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,40 +1,22 @@
+import { useLatestItems } from "../../api/itemApi.js";
 import "./Home.css";
 
-const products = [
-  {
-    name: "Callaway Glove Tour Authentic",
-    image: "https://example.com/callaway-glove.jpg",
-    price: "$29.99",
-  },
-  {
-    name: "TravisMathew Wanderlust Shorts",
-    image: "https://example.com/wanderlust-shorts.jpg",
-    price: "$79.99",
-  },
-  {
-    name: "Titleist Pro V1 Golf Balls",
-    image: "https://example.com/prov1-balls.jpg",
-    price: "$49.99",
-  },
-  {
-    name: "TaylorMade Stealth 2 Driver",
-    image: "https://example.com/stealth2-driver.jpg",
-    price: "$599.99",
-  },
-];
+
 
 
 
 export default function Home() {
+  const {latestItems } = useLatestItems();
+
   return (
     <div className="home-container">
       <h1>Welcome to the Proshop</h1>
       <div className="products-grid">
-        {products.map((product, index) => (
-          <div key={index} className="product-card">
-            <img src={product.image} alt={product.name} className="product-image" />
-            <h2>{product.name}</h2>
-            <p className="price">{product.price}</p>
+        {latestItems.map(items => (
+          <div key={items._id} className="product-card">
+            <img src={items.img} alt={items.name} className="product-image" />
+            <h2>{items.name}</h2>
+            <p className="price">{items.price}$</p>
             <button className="buy-button">Add to Cart</button>
           </div>
         ))}
