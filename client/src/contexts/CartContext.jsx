25de60ed860f71448374d848f884b1cb4e5a8815
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-31 01:33:49 +0300

    adding cartContext
    hopefully can create a working cart component that will communicate with the server

---
+++ client/src/contexts/CartContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (item) => {
+        setCart((prevCart) => {
+            const existingItem = prevCart.find((cartItem) => cartItem._id === item._id);
+            if (existingItem) {
+                return prevCart.map((cartItem) =>
+                    cartItem._id === item._id
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                        : cartItem
+                );
+            }
+            return [...prevCart, { ...item, quantity: 1 }];
+        });
+    };
+
+    const removeFromCart = (id) => {
+        setCart((prevCart) => prevCart.filter((item) => item._id !== id));
+    };
+
+    const totalPrice = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, totalPrice }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export function useCart() {
+    return useContext(CartContext);
+}
