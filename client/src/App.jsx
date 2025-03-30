e91ac0f3991a8d29626c7d2dc51b0fdb6beb7aad
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-31 01:35:15 +0300

    adding the cartContext to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import AuthGuard from './guards/AuthGuard.jsx'
 import GuestGuard from './guards/GuestGuard.jsx'
 
 import UserProvider from './providers/UserProvider.jsx'
+import { CartProvider } from './contexts/CartContext.jsx'
 
 import Footer from './components/footer/Footer.jsx'
 import Header from './components/header/Header.jsx'
@@ -17,28 +18,32 @@ import Logout from './components/logout/Logout.jsx'
 import Catalog from './components/catalog/Catalog.jsx'
 import ItemDetails from './components/details/ItemDetails.jsx'
 import ItemEdit from './components/editItem/ItemEdit.jsx'
+import CartComponent from './components/cart/CartComponent.jsx'
 
 function App() {
 
   return (
     <>
       <UserProvider>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/items' element={<Catalog />} />
-          <Route path='/items/:itemId/details' element={<ItemDetails />} />
-          <Route element={<GuestGuard />}>
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Route>
-          <Route element={<AuthGuard />}>
-            <Route path='/items/create' element={<CreateItem />} />
-            <Route path="/items/:itemId/edit" element={<ItemEdit />} />
-            <Route path="/logout" element={<Logout />} />
-          </Route>
-        </Routes>
-        <Footer />
+        <CartProvider>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/items' element={<Catalog />} />
+            <Route path='/items/:itemId/details' element={<ItemDetails />} />
+            <Route element={<GuestGuard />}>
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Route>
+            <Route element={<AuthGuard />}>
+              <Route path='/items/create' element={<CreateItem />} />
+              <Route path="/items/:itemId/edit" element={<ItemEdit />} />
+              <Route path="cart" element={<CartComponent />} />
+              <Route path="/logout" element={<Logout />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </CartProvider>
       </UserProvider>
     </>
   )
