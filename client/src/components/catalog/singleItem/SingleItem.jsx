21545776528596f4d0266e79d3dfe5309bde2fea
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-31 01:32:38 +0300

    adding some more properties in the single item

--- client/src/components/catalog/singleItem/SingleItem.jsx
+++ client/src/components/catalog/singleItem/SingleItem.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router";
+import { useCart } from "../../../contexts/CartContext.jsx";
 
 export default function SingleItem({
     _id,
     img,
     name,
-    price
+    price,
+    brand,
+    model
 }) {
+
+    const { addToCart } = useCart();
+
     return (
         <>
             <div key={_id} className="catalog-item">
                 <img src={img} alt={name} />
                 <h3>{name}</h3>
                 <p>{price}$</p>
-                <button className="add tocard">Add to Cart</button>
+                <button className="add tocard" onClick={() => addToCart({ _id, img, name, price, brand, model })}>Add to Cart</button>
                 <Link to={`/items/${_id}/details`} className="details-button">Details</Link>
             </div>
         </>
