19ab2405c272bc82403871f9c5b626680deaa3d4
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-04-01 16:05:20 +0300

    creating image for google cloud docker

--- server/server.js
+++ server/server.js
@@ -1761,7 +1761,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = process.env.PORT || 3030;
 
     server.listen(port);
 
